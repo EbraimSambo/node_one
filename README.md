dfaaea294f732c8d5e7fac2d45828dd5ce20e914
Ebraim Da Costa Sambo <107747841+EbraimSambo@users.noreply.github.com>
2024-01-22 22:52:13 +0000

    basic

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
 # node_one
+
+## Erro de iniciante
+
+antes de executar ou criar um projecte comece com npm init 
+
+## adicionar sempre a flag
+
+"dev": "node --watch --no-warnings server.js"
+
+## comandos node
+  node --watch [] 
--- server.js
+++ server.js
@@ -1,1 +1,30 @@
-import { createServer } from "node:http";
+// import { createServer } from "node:http";
+
+// const server = createServer((request, response)=>{
+//     response.write("Hello Node")
+
+//     return response.end()
+// })
+
+// server.listen(3333)
+
+
+import { fastify } from "fastify";
+
+const server = fastify()
+
+server.get('/', ()=>{
+    return "Ola Fastify"
+})
+
+server.get('/hello', ()=>{
+    return "Ola Mundo"
+})
+
+server.get('/node', ()=>{
+    return "Ola Node"
+})
+
+server.listen({
+    port: 3333,
+})
